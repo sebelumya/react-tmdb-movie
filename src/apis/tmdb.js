771e50723d92cebd5bb8a16e79e2306d6106751b
sebelumya <hamzahmuhajirin@gmail.com>
2022-07-14 19:44:24 +0700

    rm api k

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "a31608613c0c3a13af9b4967367dc110";
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const baseUrl = "https://api.themoviedb.org/3/";
 
 const tmdb = axios.create({
